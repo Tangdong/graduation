433e0db8cbe98bf596fa2c53f764d8e47e01f65f
unknown <you@example.com>
2021-03-29 14:47:13 +0800

    测试文件

---
+++ comnQss.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <qtwidgets>
+
+extern QString qssBtnInLoginWel;
+extern QString qssLeInLoginWel;
+extern QString qssLeInLoginWelError;
+extern QString qssLeLoginErrTip;
+extern QString qssBorderBtn;
+extern QString qssBorderBtn12px;
+extern QString qssMenu;
+extern QString qssBtnOrange;
+extern QString strCheckBoxQss;
+extern QString qssBtnOrange12px;
+extern QString qssBlueBorderBtn12px;
+
+extern const QString qssScrollBar();    //任务列表滚动条
+
+  
